c3bf2023a2f60b1a99462932b6d44ada554260b8
mj3876 <ms8405493@gmail.com>
2026-04-29 16:12:52 +0900

    Delete bookflow-azure-iac/BookFlowAI-Apps/glue-jobs/raw-pos-mart directory

--- bookflow-azure-iac/BookFlowAI-Apps/glue-jobs/raw-pos-mart/main.py
+++
@@ -1,5 +0,0 @@
-import sys
-from awsglue.utils import getResolvedOptions
-
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'github_sha', 's3_bucket'])
-print(f"raw-pos-mart job: SHA={args.get('github_sha')}, bucket={args.get('s3_bucket')}")
